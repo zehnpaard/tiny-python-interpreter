e825fcf102af06ef0ce8c887850fec9f571e5aa9
zehnpaard <zehnpaard@googlemail.com>
2018-09-26 08:51:51 +0900

    Implement run method of interpreter class

--- interpreter.py
+++ interpreter.py
@@ -14,3 +14,16 @@ class Interpreter:
         n2 = self.stack.pop()
         total = n1 + n2
         self.stack.append(total)
+
+    def run(self, code):
+        instructions = code['instructions']
+        numbers = code['numbers']
+        for step in instructions:
+            instruction, arg = step
+            if instruction == 'LOAD_VALUE':
+                n = numbers[arg]
+                self.LOAD_VALUE(n)
+            elif instruction == 'ADD_TWO_VALUES':
+                self.ADD_TWO_VALUES()
+            elif instruction == 'PRINT_ANSWER':
+                self.PRINT_ANSWER()
